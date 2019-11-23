d3fabbd5a2521254431f87673361c536e3fc3e8e
Sue Holder <susanpholder@gmail.com>
2019-11-23 19:04:29 +0000

    average salary graph, titles and styling added

--- static/js/graph.js
+++ static/js/graph.js
@@ -4,10 +4,14 @@ queue()
     
 function makeGraphs(error, salaryData) {
     var ndx = crossfilter(salaryData);
+
+    salaryData.forEach(function(d){
+        d.salary = parseInt(d.salary);
+    })
     
     show_discipline_selector(ndx);
     show_gender_balance(ndx);
-    
+    show_average_salary(ndx);
     
     dc.renderAll();
 }
@@ -38,4 +42,50 @@ function show_gender_balance(ndx) {
         .yAxis().ticks(20);
 }
 
+function show_average_salary(ndx) {
+    var dim = ndx.dimension(dc.pluck('sex'));
+    
+    function add_item(p, v) {
+        p.count++;
+        p.total += v.salary;
+        p.average = p.total / p.count;
+        return p;
+    }
+
+    function remove_item(p, v) {
+        p.count--;
+        if(p.count == 0) {
+            p.total = 0;
+            p.average = 0;
+        } else {
+            p.total -= v.salary;
+            p.average = p.total / p.count;
+        }
+        return p;
+    }
+    
+    function initialise() {
+        return {count: 0, total: 0, average: 0};
+    }
+
+    var averageSalaryByGender = dim.group()
+    .reduce(add_item, remove_item, initialise);
+
+    dc.barChart("#average-salary")
+        .width(400)
+        .height(300)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(dim)
+        .group(averageSalaryByGender)
+        .valueAccessor(function(d){
+            return d.value.average.toFixed(2);
+        })
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("Gender")
+        .yAxis().ticks(4);
+
+}
 
