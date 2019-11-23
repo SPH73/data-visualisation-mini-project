4713d8c2657a4c755997df4566b4e418d54bc977
Sue Holder <susanpholder@gmail.com>
2019-11-23 16:51:48 +0000

    gender balance & discipline selector graphs

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,30 +1,41 @@
 queue()
-.defer(d3.csv, "data/Salaries.csv")
-.await(makeGraphs);
-
-function makeGraphs(error, salaryData){
+    .defer(d3.csv, "data/Salaries.csv")
+    .await(makeGraphs);
+    
+function makeGraphs(error, salaryData) {
     var ndx = crossfilter(salaryData);
-
+    
+    show_discipline_selector(ndx);
     show_gender_balance(ndx);
-
+    
+    
     dc.renderAll();
-}   
+}
+
+function show_discipline_selector(ndx) {
+    var dim = ndx.dimension(dc.pluck('discipline'));
+    var group = dim.group();
+    
+    dc.selectMenu("#discipline-selector")
+        .dimension(dim)
+        .group(group);
+}
 
 function show_gender_balance(ndx) {
     var dim = ndx.dimension(dc.pluck('sex'));
     var group = dim.group();
-
+    
     dc.barChart("#gender-balance")
-    .width(400)
-    .height(300)
-    .margins({top:10, right: 50, bottom: 30, left:50})
-    .dimension(dim)
-    .group(group)
-    .transitionDuration(500)
-    .x(d3.scale.ordinal())
-    .xUnits(dc.units.ordinal)
-    .elasticY(true)
-    .xAxisLabel("Gender")
-    .yAxis().ticks(20);
+        .width(400)
+        .height(300)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Gender")
+        .yAxis().ticks(20);
 }
+
 
